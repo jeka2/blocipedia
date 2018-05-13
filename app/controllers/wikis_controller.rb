e7092806a9745dd85401c5866a0f134185078c22
Yevgeniy Ivanov <anigeka@gmail.com>
2018-05-12 20:00:45 -0500

    slack help

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,35 @@
+class WikisController < ApplicationController
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+     @wiki = Topic(wiki_params)
+
+     if @wiki.save
+       redirect_to @wiki, notice: "The wiki was saved successfully."
+     else
+       flash.now[:alert] = "Error creating topic. Please try again."
+       render :new
+     end
+   end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title,:body,:private)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :wikis
+
   devise_for :users
 
-  root 'welcome#index'
+  root 'wikis#index'
 
 end
