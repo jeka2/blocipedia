7d383ae30025bef7a81823f09726bfae75ade153
Yevgeniy Ivanov <anigeka@gmail.com>
2018-05-15 19:10:39 -0500

    slack 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :wikis, dependent: :destroy
-  after_initialize :set_user_default
+  after_initialize { self.role ||= :standard }
 
   enum role: [:standard,:premium,:admin]
-
-  private
-
-  def set_user_default
-    if current_user
-      self.role ||= standard
-    end
-  end
 
 end
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -23,7 +23,7 @@ class ApplicationPolicy
   end
 
   def update?
-    user.present?
+    user.present
   end
 
   def edit?
