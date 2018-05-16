6f6b4b05925571255ba6b0678f6203b40b44a965
Yevgeniy Ivanov <anigeka@gmail.com>
2018-05-16 16:36:03 -0500

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  after_action :set_user
   include Pundit
 
-  private
-  def set_user
-    curr_user(current_user)
-  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
-  def curr_user(user)
-    @curr_id = user.id
-  end
 
-  def current_user_id
-    @curr_id
-  end
 end
