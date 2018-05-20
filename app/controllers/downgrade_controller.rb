a04bb4cdf256c5363982d192118f80efdf51931f
Yevgeniy Ivanov <anigeka@gmail.com>
2018-05-19 21:34:18 -0500

    checkpoint complete

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -1,5 +1,8 @@
 class DowngradeController < ApplicationController
   def create
+    current_user.wikis.where(private: true).each do |wiki|
+      wiki.update_attributes(:private => false)
+    end
     current_user.standard!
     redirect_to root_path, notice: "You are now a standard-tier user"
   end
